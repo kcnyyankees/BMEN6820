e25025dc55be24ae988875a016f94b9405927f67
Anton Orlichenko <aorliche@gmail.com>
2021-03-03 12:01:29 -0600

    Added comments and fixed cycles for part 2

--- GroupB/Spruce_Budworm_Part2.m
+++ GroupB/Spruce_Budworm_Part2.m
@@ -1,10 +1,17 @@
 %% Part 2
 
+% Params
+% In this part beta is a function of time, perhaps due to fluctuations 
+% in the number of parameters
+
 alpha = 0.12;
 tend = 2e4;
 N0 = 1.5;
+
+% Beta is calculated in the model function...
 [t,N]=ode45('Spruce_Budworm_ODE_modBeta',[0 tend],N0,[],alpha);
 
+% And must be recalculated outside of it as well (for drawing graphs)
 f = 0.001;
 beta = 2*(cos(t*f)+1.5);
 
@@ -16,14 +23,15 @@ nCycles = 0;
 halfCycle = 0;
 
 for i=1:length(beta)
-    if ~halfCycle && beta(i) > 4.5
+    if ~halfCycle && beta(i) < 1.5
         nCycles = nCycles + 1;
         halfCycle = 1;
-    elseif halfCycle && beta(i) < 1.5
+    elseif halfCycle && beta(i) > 4.5
         halfCycle = 0;
     end 
 end
 
+% Plot the suggested figures
 figure;
 subplot(3,1,1);
 plot(t, beta);
@@ -40,8 +48,10 @@ plot(1./beta, N);
 xlabel('1/\beta');
 ylabel('Population');
 
+% Confirm number of cycles
 title(sprintf("%d Cycles", nCycles));
 
+% Title of entire plot
 sgtitle({'Cyclic Beta'; ...
      ['Parameters: \alpha=', num2str(alpha), ' N(0)=', num2str(N0)]});
 
